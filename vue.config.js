5fa9afff79fc9eb63142fa8873c80317c39c78c9
Artyom Mikhaylov <vedmaque@gmail.com>
2020-08-16 00:53:37 +0300

    update page title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/webdev-cheatsheet/' : '/',
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        // eslint-disable-next-line no-param-reassign
+        args[0].title = 'WebDev Cheatsheet';
+
+        return args;
+      });
+  },
 };
